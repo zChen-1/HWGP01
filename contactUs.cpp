9edb305fd0b9b50f0f0c9fe5d4678077861e55b8
Paul <ttogom@gmail.com>
2020-11-07 01:43:49 -0800

    Add files via upload

---
+++ contactUs.cpp
@@ -0,0 +1,15 @@
+#include "contactUs.h"
+#include "ui_contactUs.h"
+
+ContactUs::ContactUs(QWidget *parent)
+    : QDialog(parent),
+      ui(new Ui::ContactUs)
+{
+    ui->setupUi(this);
+}
+
+ContactUs::~ContactUs()
+{
+    delete ui;
+}
+
---
+++ contactUs.h
@@ -0,0 +1,20 @@
+#ifndef CONTACTUS_H
+#define CONTACTUS_H
+
+#include <QDialog>
+
+namespace Ui { class ContactUs; }
+
+class ContactUs : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ContactUs(QWidget *parent = nullptr);
+    ~ContactUs();
+
+private:
+    Ui::ContactUs *ui;
+};
+
+#endif // CONTACTUS_H
