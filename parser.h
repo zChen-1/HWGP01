52c14fccf2aba6fc8c6178ab24d42e748678c2b2
Paul <ttogom@gmail.com>
2020-11-10 17:15:12 -0800

    Update parser.h

--- parser.h
+++ parser.h
@@ -4,27 +4,28 @@
 #include <iostream>
 #include <fstream>
 #include "vector.h"
-#include "shape.h"
-#include "line.h"
-#include "circle.h"
-#include "polygon.h"
-#include "polyline.h"
-#include "rectangle.h"
-#include "square.h"
-#include "ellipse.h"
-#include "text.h"
+// Shape, Line, Circle... etc has to be done
+// #include "shape.h"
+// #include "line.h"
+// #include "circle.h"
+// #include "polygon.h"
+// #include "polyline.h"
+// #include "rectangle.h"
+// #include "square.h"
+// #include "ellipse.h"
+// #include "text.h"
 
 using namespace std;
 
 gp::vector<Shape*> ParseFile(int size);
-Shape* readLine(ifstream &inFile, int id);
-Shape* readPolyLine(ifstream &inFile, int id);
-Shape* readPolygon(ifstream &inFile, int id);
-Shape* readRectangle(ifstream &inFile, int id);
-Shape* readSquare(ifstream &inFile, int id);
-Shape* readEllipse(ifstream &inFile, int id);
-Shape* readCircle(ifstream &inFile, int id);
-Shape* ReadText(ifstream& inFile,int id);
+Shape* readLine(ifstream &in, int id);
+Shape* readPolyLine(ifstream &in, int id);
+Shape* readPolygon(ifstream &in, int id);
+Shape* readRectangle(ifstream &in, int id);
+Shape* readSquare(ifstream &in, int id);
+Shape* readEllipse(ifstream &in, int id);
+Shape* readCircle(ifstream &in, int id);
+Shape* ReadText(ifstream& in,int id);
 QColor getColor(string color);
 PenStyle getPenStyle(string style);
 BrushStyle getBrushStyle(string brush);
